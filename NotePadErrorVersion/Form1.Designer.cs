31d779ac4c35cbaee43f35ad0d9fc677a8dfda89
Admin <Admin@ADMIN>
2022-06-09 09:19:31 +0700

    Add project files.

---
+++ NotePadErrorVersion/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace NotePadErrorVersion
+{
+    partial class NotePadName
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileMenu = new System.Windows.Forms.MenuStrip();
+            this.Information = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FileMenu
+            // 
+            this.FileMenu.AllowMerge = false;
+            this.FileMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.FileMenu.Location = new System.Drawing.Point(0, 28);
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new System.Drawing.Size(800, 24);
+            this.FileMenu.TabIndex = 0;
+            this.FileMenu.Text = "File";
+            // 
+            // Information
+            // 
+            this.Information.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Information.Location = new System.Drawing.Point(0, 31);
+            this.Information.Multiline = true;
+            this.Information.Name = "Information";
+            this.Information.Size = new System.Drawing.Size(800, 416);
+            this.Information.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewFile,
+            this.OpenFile,
+            this.SaveFile,
+            this.ExitFile});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(46, 24);
+            this.MenuFile.Text = "File";
+            // 
+            // NewFile
+            // 
+            this.NewFile.Name = "NewFile";
+            this.NewFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewFile.Size = new System.Drawing.Size(194, 26);
+            this.NewFile.Text = "New";
+            this.NewFile.Click += new System.EventHandler(this.newFile_Click);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.OpenFile.Size = new System.Drawing.Size(194, 26);
+            this.OpenFile.Text = "Open... ";
+            this.OpenFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SaveFile.Size = new System.Drawing.Size(194, 26);
+            this.SaveFile.Text = "Save";
+            this.SaveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // ExitFile
+            // 
+            this.ExitFile.Name = "ExitFile";
+            this.ExitFile.Size = new System.Drawing.Size(194, 26);
+            this.ExitFile.Text = "Exit";
+            this.ExitFile.Click += new System.EventHandler(this.exitFile_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // NotePadName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Information);
+            this.Controls.Add(this.FileMenu);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.FileMenu;
+            this.Name = "NotePadName";
+            this.Text = "NotePad *";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip FileMenu;
+        private System.Windows.Forms.TextBox Information;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem NewFile;
+        private System.Windows.Forms.ToolStripMenuItem OpenFile;
+        private System.Windows.Forms.ToolStripMenuItem SaveFile;
+        private System.Windows.Forms.ToolStripMenuItem ExitFile;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ NotePadErrorVersion/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotePadErrorVersion
+{
+    public partial class NotePadName : Form
+    {
+        string path = "NotePad *";
+        public NotePadName()
+        {
+            InitializeComponent();
+        }
+
+        private void newFile_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(Information.Text))
+            {
+                exitPrompt();
+                if (DialogResult == DialogResult.Yes)
+                {
+                    saveFile_Click(sender, e);
+                    Information.Text = String.Empty;
+                    path = "NotePad *";
+                }
+                else if (DialogResult == DialogResult.No)
+                {
+                    Information.Text = String.Empty;
+                    path = "NotePad *";
+                }
+                this.Text = "NotePad *";
+            }
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Information.Text))
+            {
+                newFile_Click(sender, e);
+            }
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Information.Text = File.ReadAllText(path = openFileDialog1.FileName);
+                string[] fullPath = path.Split("\\");
+                path = fullPath[fullPath.Length - 1];
+                this.Text = path;
+             }
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(Information.Text))
+            {
+
+                File.WriteAllText(openFileDialog1.FileName,Information.Text);
+            }
+            if (path.CompareTo("NotePad *") == 0)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog1.FileName, Information.Text);
+                }
+            }
+            
+        }
+        private void exitFile_Click(object sender, EventArgs e)
+        {
+            exitPrompt();
+        }
+
+        private void exitPrompt()
+        {
+            DialogResult = MessageBox.Show("Do you want to save file?",
+                "NotePad Error Version",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2);
+        }
+    }
+}
